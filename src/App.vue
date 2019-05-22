82952f99dacb9faa0eca2db33686edafe7378db7
Anderson Casimiro <duodraco@gmail.com>
2019-05-21 21:46:06 -0300

    adicionando link para o projeto no github

--- src/App.vue
+++ src/App.vue
@@ -3,15 +3,15 @@
     <v-toolbar app>
       <v-toolbar-title class="headline text-uppercase">
         <span>Vingadores</span>
-        <span class="font-weight-light">Avante</span>
+        <span class="font-weight-light"> Avante</span>
       </v-toolbar-title>
       <v-spacer></v-spacer>
       <v-btn
         flat
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
+        href="https://github.com/duodraco/mercure-subscriber"
         target="_blank"
       >
-        <span class="mr-2">&nbsp;</span>
+        <v-icon color="#369">launch</v-icon> github
       </v-btn>
     </v-toolbar>
 
